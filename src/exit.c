db059130429522187a424d8066a537defbcfbb8e
Tulece <antoine.porceddu@hotmail.ca>
2024-07-25 12:11:13 +0200

    fixed invalid read

--- src/exit.c
+++ src/exit.c
@@ -1,19 +1,31 @@
 #include "../includes/cub3d.h"
 
+void free_map(char **map)
+{
+	int i;
+
+	if (!map)
+		return;
+	i = -1;
+	while (map[++i])
+		free(map[i]);
+	free(map);
+}
+
 int	quit(t_data *data)
 {
-	// free textures + datas
-	mlx_destroy_window(data->mlx, data->win);
-	mlx_destroy_display(data->mlx);
-	free(data->mlx);
+	if (data->map.map)
+		free_map(data->map.map);
+	if (data->mlx && data->win)
+		mlx_destroy_window(data->mlx, data->win);
+	if (data->mlx)
+		mlx_destroy_display(data->mlx);
+	if (data->mlx)
+		free(data->mlx);
 	exit(0);
 }
 
 int	cross_quit(t_data *data)
 {
-	// free textures + datas
-	mlx_destroy_window(data->mlx, data->win);
-	mlx_destroy_display(data->mlx);
-	free(data->mlx);
-	exit(0);
+	return (quit(data));
 }
